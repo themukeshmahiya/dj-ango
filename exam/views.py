8fc7e3cd3d20e66e49e63523d29e43da49b3a220
Mukesh Kumar <themukeshmahiya@gmail.com>
2023-06-24 15:20:49 +0530

    templates

--- exam/views.py
+++ exam/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
 def testpaper(request):
-    p = "<h1>Testpaper from exam</h1>"
-    return HttpResponse(p)
+    template = loader.get_template('testpaper.html')
+    res = template.render()
+    return HttpResponse(res)
 
 def result(request):
     r = "<h1>aagyo result tik k bath</h1>"
